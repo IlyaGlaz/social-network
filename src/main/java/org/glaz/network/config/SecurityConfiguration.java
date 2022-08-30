faa723fbfe8eff6de40289f7f9e7d79538bb33c3
glaz_ilia <glazterix@gmail.com>
2022-08-30 17:31:25 +0300

    oauth2 add

--- src/main/java/org/glaz/network/config/SecurityConfiguration.java
+++ src/main/java/org/glaz/network/config/SecurityConfiguration.java
@@ -1,15 +1,28 @@
 package org.glaz.network.config;
 
+import lombok.AllArgsConstructor;
 import org.glaz.network.database.entity.enums.UserType;
-import org.springframework.context.annotation.Bean;
+import org.glaz.network.service.UserService;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.oidc.userinfo.OidcUserRequest;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserService;
+import org.springframework.security.oauth2.core.oidc.user.DefaultOidcUser;
+import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.Set;
 
 @Configuration
+@EnableMethodSecurity
+@AllArgsConstructor
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    private final UserService userService;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -27,11 +40,28 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                         .deleteCookies("JSESSIONID"))
                 .formLogin(login -> login
                         .loginPage("/login")
-                        .defaultSuccessUrl("/users"));
+                        .defaultSuccessUrl("/users"))
+                .oauth2Login(config -> config
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/users")
+                        .userInfoEndpoint(userInfo -> userInfo.oidcUserService(oidcUserService()))
+                );
     }
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    private OAuth2UserService<OidcUserRequest, OidcUser> oidcUserService() {
+        return userRequest -> {
+            String email = userRequest.getIdToken().getClaim("email");
+            // TODO: 29.08.2022 create user userService.create
+            UserDetails userDetails = userService.loadUserByUsername(email);
+//            new OidcUserService().loadUser()
+            DefaultOidcUser oidcUser = new DefaultOidcUser(userDetails.getAuthorities(), userRequest.getIdToken());
+
+            Set<Method> userDetailsMethods = Set.of(UserDetails.class.getMethods());
+            return (OidcUser) Proxy.newProxyInstance(SecurityConfiguration.class.getClassLoader(),
+                    new Class[]{UserDetails.class, OidcUser.class},
+                    (proxy, method, args) -> userDetailsMethods.contains(method)
+                            ? method.invoke(userDetails, args)
+                            : method.invoke(oidcUser, args));
+        };
     }
 }
